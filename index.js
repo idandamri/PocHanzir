b0b517892f29bf3957ea1f0bb64a9daff9f68c23
idandamri <idandamri@gmail.com>
2019-10-30 11:36:43 +0200

    updated text log

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.post('/new-message', function(req, res) {
 })
 
 // Finally, start our server
-app.listen(88, function() {
-  console.log('Telegram app listening on port 88!')
+app.listen(3000, function() {
+  console.log('Telegram app listening on port 3000!')
 })
