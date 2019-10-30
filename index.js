ad7df4c914894cd8d9e8832e5c447f57a82a89c0
idandamri <idandamri@gmail.com>
2019-10-30 13:22:04 +0200

    port 80

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.post('/new-message', function(req, res) {
 })
 
 // Finally, start our server
-app.listen(3000, function() {
-  console.log('Telegram app listening on port 3000!')
+app.listen(80, function() {
+  console.log('Telegram app listening on port 80!')
 })
